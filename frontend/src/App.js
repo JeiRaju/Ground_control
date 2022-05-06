ccc5377e7fa7d9129d4568c1d0acf20374b80872
Jei <djjei123@gmail.com>
2022-05-06 18:27:40 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import PermanentDrawerLeft from './components/sidebar/.jsx';
 import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <PermanentDrawerLeft />
       <Dashboard />
     </div>
   );
